8c45feded402fc95d2fb90392bb61bc2159009be
s333770 <48990117+s333770@users.noreply.github.com>
2022-09-11 19:54:20 +0200

    First work on course

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: WeeAlloc= wee_alloc::WeeAlloc::INIT;
+
+struct SnakeCell(usize);
+
+struct Snake{
+    body: Vec<SnakeCell>
+}
+
+
+impl Snake{
+    fn new(spawn_index: usize)->Snake{
+        Snake{
+            body: vec!(SnakeCell(spawn_index))
+        }
+
+    }
+}
+
+#[wasm_bindgen]
+pub struct World{
+     width: usize,
+     snake: Snake,
+     size: usize
+}
+#[wasm_bindgen]
+impl World{
+    pub fn new()->World{
+        let width=8;
+        World{
+            width: 8,
+            size: width*width,
+            snake: Snake::new(10)
+        }
+
+    }
+    pub fn width(&self)->usize{
+        self.width
+    }
+    pub fn snake_head_idx(&self)->usize{
+        self.snake.body[0].0
+    }
+    pub fn update(&mut self){
+        let snake_idx=self.snake_head_idx();
+        self.snake.body[0].0=(snake_idx+1)%(self.size);
+    }
+}
+
---
+++ www/index.js
@@ -0,0 +1,56 @@
+import init, {World} from "snake-game"; 
+
+init().then(_=>{
+    const CELL_SIZE=20;
+    const world=World.new();
+    const worldWidth=world.width();
+    const canvas=document.getElementById("snake-canvas");
+    const ctx=canvas.getContext("2d");
+    canvas.height=worldWidth*CELL_SIZE;
+    canvas.width=worldWidth*CELL_SIZE;
+
+    function drawWorld(){
+        ctx.beginPath();
+        for(let x=0; x<worldWidth+1; x++){
+            ctx.moveTo(CELL_SIZE*x,0);
+            ctx.lineTo(CELL_SIZE*x, worldWidth*CELL_SIZE);
+        }
+
+        for(let y=0; y<worldWidth+1; y++){
+            ctx.moveTo(0,CELL_SIZE*y);
+            ctx.lineTo(worldWidth*CELL_SIZE, CELL_SIZE*y);
+        }
+        ctx.stroke();
+    }
+function drawSnake(){
+    const snakeIdx=world.snake_head_idx();
+    const col=snakeIdx%worldWidth;
+    const row=Math.floor(snakeIdx/worldWidth);
+    ctx.beginPath();
+    ctx.fillRect(
+        col*CELL_SIZE,
+        row*CELL_SIZE,
+        CELL_SIZE,
+        CELL_SIZE
+    );
+    ctx.stroke();
+}
+
+    function paint(){
+        drawWorld();
+        drawSnake();
+    }
+    function update(){
+        setTimeout(()=>{
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            world.update();
+            paint();
+            requestAnimationFrame(update)
+        },200)
+    }
+    paint();
+    update();
+    
+
+   
+})
